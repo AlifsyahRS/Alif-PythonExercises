bf0064f7508d1e09efeb86d4c62658cf43b2edd2
AlifsyahRS <71362235+AlifsyahRS@users.noreply.github.com>
2020-09-21 12:58:41 +0700

    Update Exercise_1_Q2.py

--- Exercise_1_Q2.py
+++ Exercise_1_Q2.py
@@ -2,7 +2,7 @@
 student1 = float(input ("Please input the score of the first student: "))
 student2 = float(input ("Please input the score of the second student: "))
 student3 = float(input("Please input the score of the third student: "))
-average = float((student1 + student2 + student3)/3)
+average = (student1 + student2 + student3)/3
 print ("Student 1's score:", student1)
 print("Student 2's score:", student2)
 print("Student 3's score:", student3)
